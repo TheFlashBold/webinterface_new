0341d382a74d4b5e08f89515986d870a79ef4221
Tycho Holzer <tycho.holzer@vonaffenfels.de>
2018-04-20 08:30:52 +0200

    lockscreen fix

--- src/directives/lockscreen.js
+++ src/directives/lockscreen.js
@@ -13,17 +13,16 @@ module.exports = [
             template: `
                 <div class="row">
                     <div class="content-wrapper full-page-wrapper d-flex align-items-center text-center lock-screen">
-                    <div class="backhground-bg"></div>
                         <div class="card col-lg-4 ml-auto mr-auto">
                             <div class="card-body">
-                            <img ng-src="{{user.profilePicture}}" alt="">
-                            <p class="user-name text-white mb-5 pb-3">{{user.email}}</p>
-                            <form>
-                                <div class="form-group">
-                                    <input type="text" class="form-control w-75" placeholder="Password" ng-model="user.password">
-                                </div>
-                                <button type="submit" class="btn btn-primary" ng-click="unlock()">Unlock</button>
-                            </form>
+								<img ng-src="{{user.profilePicture}}" alt="">
+								<p class="user-name text-white mb-5 pb-3">{{user.email}}</p>
+								<form>
+									<div class="form-group">
+										<input type="text" class="form-control w-75" placeholder="Password" ng-model="user.password">
+									</div>
+									<button type="submit" class="btn btn-primary" ng-click="unlock()">Unlock</button>
+								</form>
                             </div>
                         </div>
                     </div>
